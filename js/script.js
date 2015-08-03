02b1b3affd8027685cd7826f8565e0090f06fb7f
Marlon Cabraca <marlon24-10@hotmail.com>
2015-08-03 16:28:19 -0600

    second version

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(function() {
+  
+    rotateImages();
+    $("#menu").load("menu.html");
+    
+});
+
+
+
+
+
+
+
+function rotateImages(){
+	 var rotator = document.getElementById('mainImage');
+    var imageDir = 'img/';                     
+    var delayInSeconds = 5;                            // set number of seconds delay
+    // list image names
+    var images = ['01.jpg', '03.jpg'];
+
+    var num = 0;
+    var changeImage = function() {
+        var len = images.length;
+        rotator.src = imageDir + images[num++];
+        if (num == len) {
+            num = 0;
+        }
+    };
+    setInterval(changeImage, delayInSeconds * 1000);   	
+}
+
+  
