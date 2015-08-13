f073e997704760587af01ec82b476c04c320b97f
Marlon <marlon24-10@hotmail.com>
2015-08-13 00:39:38 -0600

    principalmemte en reservaciones y scripts

---
+++ js/reservationScript.js
@@ -0,0 +1,31 @@
+$(function() {
+  $( "#datepicker" ).datepicker({ minDate: 0, maxDate: "+1M +0D" });
+    
+   $('#timepicker').timepicker({
+    'disableTimeRanges': [
+        ['12am', '10am'],
+        ['9pm', '11:31pm']
+    ]
+});
+
+ $("input[id^='cbx']").click(function() {
+       //If checkbox is checked then disable or enable input
+    if ($(this).is(':checked'))
+    {
+        var nextCell = $(this).closest('td').next().find('input');
+         nextCell.removeAttr("disabled"); 
+      
+    }
+    //If checkbox is unchecked then disable or enable input
+    else
+    {var nextCell = $(this).closest('td').next().find('input');
+        nextCell.attr("disabled","disabled");
+        nextCell.val("");
+    }
+});
+
+
+});
+
+
+  
